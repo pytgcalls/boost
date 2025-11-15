daf51115308dc954342989b5c39c85684cc3d041
Laky64 <iraci.matteo@gmail.com>
2025-11-15 23:16:38 +0100

    Use CLang for macOS builds

--- build.sh
+++ build.sh
@@ -1,29 +1,34 @@
 source /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/pytgcalls/build-toolkit/refs/heads/master/build-toolkit.sh)"
 
+if is_macos || (is_linux && ! is_arm64); then
+  require clang-21
+fi
+
 import libraries.properties
 import setup-clang.sh
 
 sysroot=""
 
-if is_linux; then
+if is_linux || is_macos; then
   build_and_install "libcxx" clone
   build_and_install "buildtools" clone
 
   cp "$DEFAULT_BUILD_FOLDER/buildtools/third_party/libc++/__config_site" "$DEFAULT_BUILD_FOLDER/libcxx/include/"
   cp "$DEFAULT_BUILD_FOLDER/buildtools/third_party/libc++/__assertion_handler" "$DEFAULT_BUILD_FOLDER/libcxx/include/"
-elif is_macos; then
-  sysroot="$(xcrun --sdk macosx --show-sdk-path 2>/dev/null | tr -d '[:space:]')"
+
+  if is_macos; then
+    sysroot="$(xcrun --sdk macosx --show-sdk-path 2>/dev/null | tr -d '[:space:]')"
+  fi
 fi
 
 build_and_install "boost" b2-static \
     --update-submodules \
-    --linux="target-os=linux \
-      cxxflags=\"-D_LIBCPP_ABI_NAMESPACE=Cr -D_LIBCPP_ABI_VERSION=2 -D_LIBCPP_DISABLE_AVAILABILITY -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE -nostdinc++ -isystem$DEFAULT_BUILD_FOLDER/libcxx/include\"" \
+    --linux="target-os=linux cxxflags=\"$(libcxx_flags)\"" \
     --windows="target-os=windows toolset=msvc
       runtime-link=static
       cxxflags=\"-D_ITERATOR_DEBUG_LEVEL=0\"" \
-    --macos="cflags=\"--sysroot=$sysroot -target aarch64-apple-darwin -mmacosx-version-min=12.0\" \
-      cxxflags=\"--sysroot=$sysroot -std=gnu++17 -target aarch64-apple-darwin -mmacosx-version-min=12.0\" \
+    --macos="cflags=\"--sysroot=$sysroot -target aarch64-apple-darwin -mmacosx-version-min=14.0\" \
+      cxxflags=\"--sysroot=$sysroot -std=gnu++17 -target aarch64-apple-darwin -mmacosx-version-min=14.0 $(libcxx_flags)\" \
       visibility=hidden" \
     --linux-macos="runtime-link=shared toolset=clang --pre-build-commands=\"setup_clang\"" \
     --linux-windows="visibility=global" \
--- setup-clang.sh
+++ setup-clang.sh
@@ -1,3 +1,7 @@
 function setup_clang() {
-    echo "using clang : : clang++ : ;" > "$DEFAULT_BUILD_FOLDER/boost/project-config.jam"
+    echo "using clang : : $(if ! is_arm64; then echo "$CLANG_BIN/"; fi)clang++ : ;" > "$DEFAULT_BUILD_FOLDER/boost/project-config.jam"
+}
+
+function libcxx_flags() {
+    echo "-D_LIBCPP_ABI_NAMESPACE=Cr -D_LIBCPP_ABI_VERSION=2 -D_LIBCPP_DISABLE_AVAILABILITY -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE -nostdinc++ -isystem$DEFAULT_BUILD_FOLDER/libcxx/include"
 }
